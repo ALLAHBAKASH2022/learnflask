ed4cd5cf5d5fe19bab9c0c71513a029f241b1cfa
ALLAHBAKASH2022 <102593370+ALLAHBAKASH2022@users.noreply.github.com>
2023-05-18 11:19:13 +0000

    flask app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-status
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "<h1>Hello world, How are you</h1>"
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
