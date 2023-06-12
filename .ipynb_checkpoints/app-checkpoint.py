95b967a65cf096b150f3bc2d22918377b83213b5
ALLAHBAKASH2022 <102593370+ALLAHBAKASH2022@users.noreply.github.com>
2023-06-12 04:38:13 +0000

    updated

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<h1>Hello world, How are you ali</h1>"
+    return "<h1>Hello world, How are you</h1>"
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=8081)
+    app.run(debug=True,host="0.0.0.0",port=8080)
